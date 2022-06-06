296628db414ced92456157eeabb819105cc5cc4e
ItsBhuvnesh <bhuvankumar9944@gmail.com>
2022-06-06 22:27:47 +0530

    :tada: Initial Commit

--- Utility/colors.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  success: "00bb9b",
-  error: "f34336",
-  main: "ffffff",
-};
--- models/autoReconnect.js
+++
@@ -1,9 +0,0 @@
-const { Schema, model } = require("mongoose");
-
-const AutoReconnect = new Schema({
-  ChannelID: String,
-  TextChannelID: String,
-  GuildID: String,
-});
-
-module.exports = model("autoReconnect", AutoReconnect);
